9ae16223520f9ec5c7949c0187a9cc4d284d1f8f
austin-li <austinmingli@gmail.com>
2022-01-26 18:34:40 -0800

    add JUnit test for test-file.md

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,22 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.util.*;
+import java.io.IOException;
+import java.nio.file.*;
+
 public class MarkdownParseTest {
 	@Test
 	public void addition() {
 		assertEquals(2, 1 + 1);
+	}
+
+	@Test
+	public void testFile() throws IOException {
+		Path fileName = Path.of("test-file.md");
+		String contents = "";
+		contents = Files.readString(fileName);
+		ArrayList<String> links = MarkdownParse.getLinks(contents);
+		assertEquals(List.of("https://something.com", "some-page.html"), links);
 	}
 }
